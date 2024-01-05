7846678e9ad2bca7d39f0bd73df1d3636dafabc0
matias-humacata <hhumacata@gmail.com>
2024-01-04 23:13:45 -0300

    subiendo proyecto

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import moto from "./assets/img/moto.gif";
 import logo from './logo.svg';
 import './App.css';
+import { LoginButton } from './login';
+import { LogoutButton } from "./logout";
+import { Profile } from "./profile";
 
 function App() {
+
+  const { isAuthenticated } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={moto} className="App-logo" alt="logo" />
+       {isAuthenticated ? (
+       <>
+        <Profile />
+        <LogoutButton />
+        </>
+      ) :( <LoginButton />
+  )}
       </header>
     </div>
   );
